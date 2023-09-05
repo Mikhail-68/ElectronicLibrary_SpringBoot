47f182a2d90616fb0e5156fefe5ec921d987c187
Egorov Mikhail <misha2003200@gmail.com>
2023-09-05 11:42:31 +0300

    Spring Boot initialize

---
+++ src/main/java/ru/egorov/electroniclibrary/ElectronicLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.egorov.electroniclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/egorov/electroniclibrary/ElectronicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egorov.electroniclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
